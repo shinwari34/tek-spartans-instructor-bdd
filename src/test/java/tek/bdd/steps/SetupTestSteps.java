1e2333e83d1b7a80412912b20d1e214371f7a324
Himmat <Najeebullah.shinwari34@gmail.com>
2024-07-31 17:05:21 -0600

    make code maintainable

---
+++ src/test/java/tek/bdd/steps/SetupTestSteps.java
@@ -0,0 +1,27 @@
+package tek.bdd.steps;
+import org.junit.Assert;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import tek.bdd.utility.SeleniumUtility;
+
+public class SetupTestSteps extends SeleniumUtility {
+
+    @Given("Open browser and navigate to retail app")
+    public void openBrowserAndNavigate() {
+        setupBrowser();
+    }
+
+    @Then("validate top left corner is TEKSCHOOL")
+    public void validateTopLeftLogo() {
+        String topLeftCornerText = getElementText(By.className("top-nav__logo"));
+
+        Assert.assertEquals("TEKSCHOOL" , topLeftCornerText);
+    }
+
+    @Then("Close the browser")
+    public void closeBrowser() {
+        quitBrowser();
+    }
+
+
+}
