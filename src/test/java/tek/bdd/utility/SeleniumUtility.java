b2cdf23a85ca39d5be136a5e96dd9212dac90b1b
Himmat <Najeebullah.shinwari34@gmail.com>
2024-07-31 16:30:20 -0600

    created selenium utilities class

---
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -0,0 +1,37 @@
+package tek.bdd.utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import tek.bdd.base.BaseSetup;
+
+import java.time.Duration;
+
+
+public class SeleniumUtility extends BaseSetup {
+
+    private WebDriverWait getWait() {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+    }
+
+    private WebElement waitForVisibility(By locator) {
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void clickOnElement(By locator) {
+        getWait().until(ExpectedConditions.elementToBeClickable(locator))
+                .click();
+    }
+
+    public void sendText(By locator, String value) {
+        waitForVisibility(locator).sendKeys(value);
+    }
+
+    //Create method for getting the text of a locator
+    public String getElementText(By locator) {
+        return waitForVisibility(locator).getText();
+    }
+
+}
