eb5810348181695cbd99ffc2f208c53d2e4355cb
Himmat <Najeebullah.shinwari34@gmail.com>
2024-08-01 00:28:58 -0600

    setup class completed

---
+++ src/test/java/tek/bdd/steps/HooksSteps.java
@@ -0,0 +1,18 @@
+package tek.bdd.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import tek.bdd.utility.SeleniumUtility;
+
+public class HooksSteps extends SeleniumUtility {
+
+    @Before
+    public void beforeEachScenario() {
+        setupBrowser();
+    }
+
+    @After
+    public void afterEachScenario() {
+        quitBrowser();
+    }
+}
