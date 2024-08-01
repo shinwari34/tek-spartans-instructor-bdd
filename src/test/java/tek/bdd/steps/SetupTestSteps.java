15a98d673286b17faad7ce0a11952de87e22de18
Himmat <Najeebullah.shinwari34@gmail.com>
2024-08-01 00:14:59 -0600

    parameterized steps

--- src/test/java/tek/bdd/steps/SetupTestSteps.java
+++ src/test/java/tek/bdd/steps/SetupTestSteps.java
@@ -1,26 +1,22 @@
 package tek.bdd.steps;
-import org.junit.Assert;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import tek.bdd.utility.SeleniumUtility;
 
 public class SetupTestSteps extends SeleniumUtility {
 
-    @Given("Open browser and navigate to retail app")
-    public void openBrowserAndNavigate() {
-        setupBrowser();
-    }
-
     @Then("validate top left corner is TEKSCHOOL")
     public void validateTopLeftLogo() {
         String topLeftCornerText = getElementText(By.className("top-nav__logo"));
-
         Assert.assertEquals("TEKSCHOOL" , topLeftCornerText);
     }
 
-    @Then("Close the browser")
-    public void closeBrowser() {
-        quitBrowser();
+    @Then("Validate Sign in button is Enabled")
+    public void validateSignInButtonEnabled() {
+        boolean isSignInBtnEnabled = isElementEnabled(By.id("signinLink"));
+        Assert.assertTrue(isSignInBtnEnabled);
     }
 
 
