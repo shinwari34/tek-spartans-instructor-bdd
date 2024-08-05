f90232fc4479926dbd54a5ebafc89ca7c85096ce
Himmat <Najeebullah.shinwari34@gmail.com>
2024-08-04 20:34:05 -0600

    setup class completed

--- src/test/java/tek/bdd/runners/TestRunner.java
+++ src/test/java/tek/bdd/runners/TestRunner.java
@@ -4,13 +4,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:features/SecurityTest.feature", // Directory for Feature file
         glue = "tek.bdd.steps", //Package for all the steps
         dryRun = true //set to true to scan feature for unimplemented steps
-
 )
 public class TestRunner {
 }
